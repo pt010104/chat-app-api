1952346fa88d870a663393d2e4b18a73acd224d4
pt010104 <hisokaxxzk@gmail.com>
2024-05-25 14:34:55 +0700

    Test post

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.post('/post', (req, res) => {
+  res.send('HI post!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
