61b3e63cd2637ddd0753af74d427eedaa9ccb060
pt010104 <hisokaxxzk@gmail.com>
2024-05-26 16:21:10 +0700

    Comment test

--- src/app.js
+++ src/app.js
@@ -6,30 +6,8 @@
     const router = require ("./routes/index")
     const config = require("./configs/db.config").db;
     //Test for creating new model in database
-    const User = require('./models/user_test.model');
-    const mongoose = require('mongoose');
-
-mongoose.connect('your_connection_string', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const createInitialUsers = async () => {
-  try {
-    await User.deleteMany({}); // Caution: This line deletes all existing users!
-
-    const users = [
-      { username: 'user1', email: 'user1@example.com', password: 'password123' },
-      { username: 'user2', email: 'user2@example.com', password: 'password123' }
-    ];
-
-    await User.insertMany(users);
-    console.log('Initial users created');
-  } catch (error) {
-    console.error('Error initializing the database:', error);
-  } finally {
-    mongoose.disconnect();
-  }
-};
-
-createInitialUsers();
+    // const User = require('./models/user_test.model');
+    // const mongoose = require('mongoose');
     //init middleware
     app.use(express.json())
     app.use(express.urlencoded({ extended: true }))
