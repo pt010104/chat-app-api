f81efee6fd8934887e4fdec8de573ce77c5dfffc
pt010104 <hisokaxxzk@gmail.com>
2024-05-26 08:59:41 +0700

    Error handler

---
+++ src/core/success.response.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const {StatusCodes,ReasonPhrases} = require ("../utils/httpStatusCode.js")
+
+class SuccessResponse{
+    constructor({message, statucsCode = StatusCodes.OK, reasonPhrases = ReasonPhrases.OK, metadata={} }){
+        this.message = message || reasonPhrases
+        this.status = statucsCode
+        this.metadata = metadata
+    }
+
+    send(res,headers ={}) {
+        return res.status(this.status).json(this)
+    }
+
+}
+
+class OK extends SuccessResponse{
+    constructor({message, metadata = {}}){
+        super({message, metadata})
+    }
+}
+
+class CREATED extends SuccessResponse{
+constructor({message,statucsCode = StatusCodes.CREATED, reasonPhrases = ReasonPhrases.CREATED, metadata = {}}){
+        super({message, statucsCode, reasonPhrases, metadata})
+    }
+}
+
+module.exports =  {
+    OK,
+    CREATED,
+    SuccessResponse
+}
