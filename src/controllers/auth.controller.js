d1489eaffc82f580dcf3c8121ac9ef09995641cb
nguyen2192004 <phamgianguyen.qn@gmail.com>
2024-06-03 14:03:34 +0700

    a

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,35 +1,50 @@
-'use strict'
-const {OK,CREATED,SuccessResponse} = require ("../core/success.response")
-const {BadRequestError, ForbiddenError} = require ("../core/error.response")
-const Joi = require('joi');
-const AuthService = require('../services/auth.service'); 
+"use strict";
+const { OK, CREATED, SuccessResponse } = require("../core/success.response");
+const { BadRequestError, ForbiddenError } = require("../core/error.response");
+const Joi = require("joi");
+const AuthService = require("../services/auth.service");
 
 class AuthController {
+  signUp = async (req, res, next) => {
+    const signUpValidate = Joi.object({
+      name: Joi.string().alphanum().min(3).max(30).required(),
+      password: Joi.string().min(8).max(30).required(),
+      email: Joi.string().email().required(),
+    });
 
-    signUp = async (req, res, next) => { 
+    const { error } = signUpValidate.validate(req.body);
 
-        const signUpValidate = Joi.object({
-            name: Joi.string().alphanum().min(3).max(30).required(),
-            password: Joi.string().min(8).max(30).required(),
-            email: Joi.string().email().required(),
-        });
-
-        const { error } = signUpValidate.validate(req.body);    
-
-        if (error) {
-            return res.status(400).json({
-                message: error.details[0].message
-            });
-        }    
-
-        const {body} = req;
-        new CREATED (
-            {
-                message: "User created successfully",
-                metadata: await AuthService.signUp(body)
-            }
-        ).send(res)
-
+    if (error) {
+      return res.status(400).json({
+        message: error.details[0].message,
+      });
     }
+
+    const { body } = req;
+    new CREATED({
+      message: "User created successfully",
+      metadata: await AuthService.signUp(body),
+    }).send(res);
+  };
+  signIn = async (req, res, next) => {
+    const signInValidate = Joi.object({
+      password: Joi.string().min(8).max(30).required(),
+      email: Joi.string().email().required(),
+    });
+
+    const { error } = signInValidate.validate(req.body);
+
+    if (error) {
+      return res.status(400).json({
+        message: error.details[0].message,
+      });
+    } // go to authservice signin
+    else {
+      new SuccessResponse({
+        message: "User signed in successfully",
+        metadata: await AuthService.signIn(req.body),
+      }).send(res);
+    }
+  };
 }
-module.exports = new AuthController()
+module.exports = new AuthController();
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,17 +1,16 @@
-'use strict'
+"use strict";
 
-const express = require ("express")
-const router = express.Router()
-const AuthController = require ("../../controllers/auth.controller")
-const {asyncHandler} = require("../../helpers/asyncHandler")
-const {authentication} = require("../../auth/authUtils")
+const express = require("express");
+const router = express.Router();
+const AuthController = require("../../controllers/auth.controller");
+const { asyncHandler } = require("../../helpers/asyncHandler");
+const { authentication } = require("../../auth/authUtils");
 
 //register
-router.post ("/signup", asyncHandler(AuthController.signUp))
-
-router.use(authentication)
+router.post("/signup", asyncHandler(AuthController.signUp));
+router.post("/signin", asyncHandler(AuthController.signIn));
+router.use(authentication);
 
 //Logout and forgot password here
 
-module.exports = router
-
+module.exports = router;
