208b8eb14ad4c37b72a0695666d6577568067f20
pt010104 <hisokaxxzk@gmail.com>
2024-06-04 13:03:23 +0700

    Remove SIGINT from Server

--- server.js
+++ server.js
@@ -4,7 +4,3 @@ const PORT = 5050
 const server = app.listen (PORT, () =>{
     console.log(`Start with ${PORT}`)
 })
-
-process.on('SIGINT', ()=>{
-    server.close( () => console.log("Exist server express") )
-})
