9591ac25dc4badf31cae6eb27175ef4a74b997a3
pt010104 <hisokaxxzk@gmail.com>
2024-06-06 14:50:43 +0700

    Git merge + add authen for profile

--- src/routes/profile/index.js
+++ src/routes/profile/index.js
@@ -4,7 +4,10 @@ const express = require("express");
 const router = express.Router();
 const ProfileController = require("../../controllers/profile.controller");
 const { asyncHandler } = require("../../helpers/asyncHandler");
+const { authentication } = require("../../auth/authUtils");
+
 //register
+router.use(authentication)
 router.get("/info", asyncHandler(ProfileController.infoProfile));
 
 
