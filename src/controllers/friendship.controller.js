50c1261626b5920806d920c099049e0db8d4928a
pt010104 <hisokaxxzk@gmail.com>
2024-06-22 13:52:28 +0700

    Set up friend model + [API] List Friends Request (Invitations) [API] Send Friend Request [API] Accept Friend Request [API] Cancel Request

---
+++ src/controllers/friendship.controller.js
@@ -0,0 +1,99 @@
+'use strict'
+const { BadRequestError } = require('../core/error.response')
+const { SuccessResponse } = require('../core/success.response')
+const FriendShip = require('../services/friendship.service')
+const Joi = require("joi");
+
+class FriendshipController {
+
+    // v1/api/friends/list/all
+    listFriends = async(req, res, next) => {
+    }
+
+    // v1/api/friends/list/requests
+    listRequestsFriends = async(req, res, next) =>  {
+        const user_id = req.user.userId
+        new SuccessResponse({
+            message: "List friend requests",
+            metadata: await FriendShip.listRequestsFriends(user_id)
+        }).send(res)
+    }
+
+    // v1/api/friends/send-request
+    sendFriendRequest = async(req, res, next) =>  {
+         
+        const friendValidate = Joi.object({
+            user_id_recieve: Joi.string().required()
+        });
+
+        const { error } = friendValidate.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+              message: error.details[0].message,
+            });
+        }
+
+        const user_id = req.user.userId
+        const { user_id_recieve } = req.body
+        new SuccessResponse({
+            message: "Friend request sent",
+            metadata: await FriendShip.sendFriendRequest(user_id, user_id_recieve)
+        }).send(res)
+    }
+
+    // v1/api/friends/accept
+    acceptFriendRequest = async(req, res, next) =>  {
+
+        const friendValidate = Joi.object({
+            user_id_send: Joi.string().required()
+        });
+
+        const { error } = friendValidate.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+              message: error.details[0].message,
+            });
+        }
+
+        const user_id = req.user.userId
+        const { user_id_send } = req.body
+        new SuccessResponse({
+            message: "Friend request accepted",
+            metadata: await FriendShip.acceptFriendRequest(user_id, user_id_send)
+        }).send(res)
+    }
+
+    // v1/api/friends/cancel-request
+    cancelFriendRequest = async(req, res, next) => {
+
+        const friendValidate = Joi.object({
+            user_id_recieve: Joi.string().required()
+        });
+
+        const { error } = friendValidate.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+              message: error.details[0].message,
+            });
+        }
+
+        const user_id = req.user.userId
+        const { user_id_recieve } = req.body
+        new SuccessResponse({
+            message: "Friend request canceled",
+            metadata: await FriendShip.cancelFriendRequest(user_id, user_id_recieve)
+        }).send(res)
+
+    }
+
+    // v1/api/friends/reject-request
+    async rejectFriendRequest(req, res) {
+    }
+
+    // v1/api/friends/remove
+    async removeFriend(req, res) {
+    }
+
+}
+
+module.exports = new FriendshipController()
---
+++ src/models/friendship.model.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const {model, Schema, Types} = require('mongoose')
+
+const DOCUMENT_NAME = 'friendship'
+const COLLECTION_NAME = 'friendships'
+
+const friendshipSchema = new Schema({
+    user_id_send: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    user_id_receive : {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    status: {type: String, default: 'pending', enum: ['pending', 'accepted', 'rejected']},
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, friendshipSchema)
