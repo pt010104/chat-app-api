75cdd501b40fdaf4ecd1fd66566651cafcccc2a5
pt010104 <hisokaxxzk@gmail.com>
2024-07-18 11:53:08 +0700

    Upload file for deploy api

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -3,6 +3,7 @@
 const cloudinary = require("../dbs/init.cloudinary");
 const CONSTANT = require('../helpers/constants');
 const User = require("../models/user.model");
+const { Readable } = require('stream'); 
 
 class UploadService {
     uploadImageFromLocal = async ({
@@ -63,6 +64,77 @@ class UploadService {
             throw new Error(error)
         }
        
+    }
+
+    uploadImageFromBuffer = async ({
+        buffer,
+        originalname,
+        user_id,
+        type = "avatar",
+    }) => {
+        try {
+            let folderName = type + "/" + user_id;
+
+            const uploadImage = await new Promise((resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    {
+                        public_id: `${user_id}_${Date.now()}`,
+                        folder: folderName,
+                    },
+                    (error, result) => {
+                        if (error) reject(error);
+                        else resolve(result);
+                    }
+                );
+
+                const readableStream = new Readable();
+                readableStream.push(buffer);
+                readableStream.push(null);
+                readableStream.pipe(uploadStream);
+            });
+
+            let url = {};
+
+            if (type == "avatar") {
+                const avt_url = await cloudinary.url(uploadImage.public_id, {
+                    width: CONSTANT.WIDTH_AVATAR,
+                    height: CONSTANT.HEIGHT_AVATAR,
+                    crop: "fill",
+                    format: 'jpg'
+                })
+                const avt_thumb_url = await cloudinary.url(uploadImage.public_id, {
+                    width: CONSTANT.WIDTH_THUMB_AVATAR,
+                    height: CONSTANT.HEIGHT_THUMB_AVATAR,
+                    crop: "fill",
+                    format: 'jpg'
+                })
+                url['avt_url'] = avt_url;
+                url['avt_thumb_url'] = avt_thumb_url;
+
+                const uploadAvt = await User.updateOne(
+                    { _id: user_id }, 
+                    {
+                        '$set': {
+                            avatar: avt_url,
+                            thumb_avatar: avt_thumb_url
+                        }
+                    }, 
+                    {
+                        upsert: true,
+                        runValidators: true,
+                    }
+                );                
+            }
+           
+            return {
+                url,
+                user_id: user_id,
+                type: type,
+            }
+        }
+        catch (error) {
+            throw new Error(error)
+        }
     }
 }
 
