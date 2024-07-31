4573e1cd11b8efa35336cbe0e5ead267b442eee4
pt010104 <hisokaxxzk@gmail.com>
2024-07-31 10:03:23 +0700

    Revert "Init model"
    This reverts commit cc4857417095a52c1616424baf48cdb614ac500f.

--- src/models/repository/user.repository.js
+++ src/models/repository/user.repository.js
@@ -2,38 +2,39 @@ const userModel = require("../user.model")
 const { BadRequestError, NotFoundError } = require("../../core/error.response")
 const RedisService = require("../../services/redis.service")
 
-class UserRepository {
-    static findUserById = async (id) => {
-        try {
-            const key = `user:${id}`
-            let userFound = await RedisService.get(key)
+const findUserById = async (id) => {
+    try {
+        const key = `user:${id}`
+        let userFound = await RedisService.get(key)
 
-            if (userFound) {
-            userFound = JSON.parse(userFound)
-            return userFound
-            }
-
-            userFound = await userModel.findOne({ _id: id }).lean()
-            if (!userFound) {
-                throw new NotFoundError("User not found")
-            }
-
-            await RedisService.set(key, JSON.stringify(userFound), 3600)
-
-            return userFound
-        } catch (error) {
-            throw new BadRequestError(error)
+        if (userFound) {
+           userFound = JSON.parse(userFound)
+           return userFound
         }
-    }
 
-    static findUserByEmail = async (email) => {
-        const userFound = await userModel.findOne({ email }).lean()
+        userFound = await userModel.findOne({ _id: id }).lean()
         if (!userFound) {
             throw new NotFoundError("User not found")
         }
-        
+
+        await RedisService.set(key, JSON.stringify(userFound), 3600)
+
         return userFound
+    } catch (error) {
+        throw new BadRequestError(error)
     }
 }
 
-module.exports = UserRepository
+const findUserByEmail = async (email) => {
+    const userFound = await userModel.findOne({ email }).lean()
+    if (!userFound) {
+        throw new NotFoundError("User not found")
+    }
+    
+    return userFound
+}
+
+module.exports = {
+    findUserById,
+    findUserByEmail
+}
