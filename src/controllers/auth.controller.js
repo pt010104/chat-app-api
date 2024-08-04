0b5352b90e42ebfe5242cd490dfa3367ae95d7ad
pt010104 <hisokaxxzk@gmail.com>
2024-08-04 20:54:06 +0700

    Change validate for user name

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,7 +8,7 @@ class AuthController {
   
   signUp = async (req, res, next) => {
     const signUpValidate = Joi.object({
-      name: Joi.string().alphanum().min(3).max(30).required(),
+      name: Joi.string().min(3).max(30).required(),
       password: Joi.string().min(8).max(30).required(),
       email: Joi.string().email().required(),
     });
