a5921839e41228e9ffc59c72c7ce33866eb0b1cf
Hải Đào <haigr2004@gmai.com>
2024-08-06 19:05:19 +0700

    finish list, remove, leave

--- src/routes/chat/index.js
+++ src/routes/chat/index.js
@@ -12,5 +12,5 @@ route.get('/messages/:room_id', asyncHandler(chatController.getMessagesInRoom));
 route.post('/add-users/:room_id', asyncHandler(chatController.addUsersToRoom));
 route.post('/remove-users/:room_id', asyncHandler(chatController.removeUsersFromRoom));
 route.post('/leave/:room_id', asyncHandler(chatController.leaveRoom));
-route.post('/list-rooms', asyncHandler(chatController.listRooms));
+route.get('/list-rooms', asyncHandler(chatController.listRooms));
 module.exports = route;
