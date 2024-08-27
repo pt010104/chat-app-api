94240ef6dcbadf692be1841c3e89bb91cd569540
Hải Đào <haigr2004@gmai.com>
2024-08-27 23:02:33 +0700

    model pin message

---
+++ src/models/pinMessage.model.js
@@ -0,0 +1,29 @@
+'use strict'
+const {model, Schema, Types} = require('mongoose')
+
+const DOCUMENT_NAME = 'pinMessage'
+const COLLECTION_NAME = 'pinMessages'
+
+const pinMessageSchema = new Schema({
+    message_id: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'chats'
+    },
+    message: {
+        type: [String],
+        required: true
+    },
+    
+    room_id: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'Room'
+    },
+    
+}, {
+    timestamps: true,
+    collection: COLLECTION_NAME
+})
+
+module.exports = model(DOCUMENT_NAME, pinMessageSchema)
