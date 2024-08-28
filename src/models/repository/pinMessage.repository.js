03184dbfd3707459fc4876ef12177057f7124ed1
Hải Đào <haigr2004@gmai.com>
2024-08-28 21:28:15 +0700

    fix id

--- src/models/repository/pinMessage.repository.js
+++ src/models/repository/pinMessage.repository.js
@@ -2,7 +2,6 @@
 
 
 const { BadRequestError } = require('../../core/error.response');
-const { findUserById } = require('./user.repository');
 const RedisService = require('../../services/redis.service');
 const { NotFoundError } = require('../../core/error.response');
 const pinMessageModel = require('../pinMessage.model');
@@ -10,19 +9,36 @@ const pinMessageModel = require('../pinMessage.model');
 
 
 class pinMessageRepository {
-    async pinMessage(room_id, message_id) {
-        const message = await pinMessageModel.findByIdAndUpdate(
-            room_id,
-            { $addToSet: { message_id } },
-            { new: true, runValidators: true }
-        );
+    pinMessage = async (room_id, message_id) => {
+
+        let message = await pinMessageModel.findById(room_id);
+
+        if (!message) {
+        
+            message = await pinMessageModel.create({
+                room_id: room_id,
+                message_id: [message_id]
+            });
+        } else {
+            // If a pin exists, update the existing entry to add the new message ID
+            message = await pinMessageModel.findByIdAndUpdate(
+                room_id,
+                { $addToSet: { message_id } },
+                { new: true, runValidators: true }
+            );
+        }
+
         if (!message) {
             throw new NotFoundError('Room not found');
         }
-        const key= 'pinMessage:' + room_id;
+
+        // Push the message ID to Redis
+        const key = `pinMessage:${room_id}`;
         await RedisService.rPush(key, message_id);
+
         return message;
     }
+
 
     unpinMessage = async (room_id, message_id) => {
         const message = await pinMessageModel.findByIdAndUpdate(
