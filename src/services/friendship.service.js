ca95509fa8d591268092e7b3b3aaa4b1e18ff945
pt010104 <hisokaxxzk@gmail.com>
2024-09-01 13:02:25 +0700

    Fix accept

--- src/services/friendship.service.js
+++ src/services/friendship.service.js
@@ -188,6 +188,11 @@ class FriendShip {
 
         ChatService.createRoom(createRoomParams)
 
+        const key1 = `listFriends:${user_id}`;
+        const key2 = `listFriends:${acceptRequest.user_id_send}`;
+        RedisService.delete(key1);
+        RedisService.delete(key2)
+
         return {
             acceptRequest
         }
