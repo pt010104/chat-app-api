d867595efbe5e7c7f685926227e9829fd016d841
pt010104 <hisokaxxzk@gmail.com>
2024-09-01 13:39:31 +0700

    Message or buffer is required

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -136,7 +136,7 @@ class ChatController {
 
     sendMessage = async (req, res, next) => {
         const sendMessageValidate = Joi.object({
-            message: Joi.string().required(),
+            message: Joi.string().optional(),
             room_id: Joi.string().required(),
             buffer: Joi.string().optional(),
         });
@@ -149,6 +149,12 @@ class ChatController {
 
         const userId = req.user.userId;
         const { message, room_id, buffer } = req.body;
+
+        if (!message && !buffer) {
+            return res.status(400).json({
+                message: "Message or buffer is required",
+            });
+        }
 
         new SuccessResponse({
             message: "Message sent successfully",
