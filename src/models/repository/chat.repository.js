f0dca691b80b036646bebfa189145a0f1f11bae3
pt010104 <hisokaxxzk@gmail.com>
2024-09-03 00:21:29 +0700

    Add id for tranform chat msg

--- src/models/repository/chat.repository.js
+++ src/models/repository/chat.repository.js
@@ -54,6 +54,9 @@ class ChatRepository {
             if (chatData.release_time) {
                 transformedData.release_time = chatData.release_time;   
             }
+            if(chatData._id) {
+                transformedData.id = chatData._id;
+            }
 
             return transformedData;
         } catch (error) {
