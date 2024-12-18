cd85d10f97935d43ab82fb1a314be30a9bdccaa5
pt010104 <hisokaxxzk@gmail.com>
2024-12-18 17:58:01 +0700

    testtt

--- src/public/MIS Final Project/Our Story/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('findStoreButton').addEventListener('click', function() {
-    window.location.href = '../Locations/index.html'; // Adjust the path as needed
-});
-
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    const dropdown = document.querySelector('.dropdown-menu');
-    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
-  });
-  
